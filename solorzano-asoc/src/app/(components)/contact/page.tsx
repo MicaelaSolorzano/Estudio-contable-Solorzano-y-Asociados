009470393de6d91fa4286f5059947a7025cc0c8b
samunu <micaolor@gmail.com>
2024-04-24 00:06:34 -0300

    creating more components

--- solorzano-asoc/src/app/(components)/contact/page.tsx
+++ solorzano-asoc/src/app/(components)/contact/page.tsx
@@ -1,14 +1,17 @@
 export default function Contact() {
   return (
-    <section>
-      <div>
-        <h4>
-          <h2>¿Necesitás un contador de confianza?</h2>
-          No dudes en contactarnos!
+    <section className="flex  min-h-screen min-w-screen justify-items-center p-10">
+      <div className="flex flex-col w-96 p-10 gap-5">
+        <h1>¿Necesita un contador de confianza?</h1>
+        <h4 className="text-justify">
+          ¡No dude en contactarnos! Puede enviarnos su consulta por medio del
+          siguiente formulario o al mail:{" "}
+          <span> info@solorzanoyasoc.com.ar </span> y a la brevedad nos
+          comunicaremos con usted.
         </h4>
       </div>
-      <div className="flex min-h-screen flex-col items-center justify-between p-24">
-        <form className="flex flex-col w-1/2 justify-between p-10 gap-5">
+      <div className="flex min-h-screen flex-col items-center ">
+        <form className="flex flex-col w-96 p-10 gap-5">
           <label>Nombre:</label>
           <input className="inpt"></input>
 
--- solorzano-asoc/src/app/page.tsx
+++ solorzano-asoc/src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Nav from "./(components)/nav/Nav";
+import Contact from "./(components)/contact/page";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen min-w-screen flex-col items-center justify-between ">
+    <main className="flex min-h-screen min-w-screen flex-col justify-between ">
       <Nav></Nav>
+      <h1 className="title">Contacto</h1>
+      <Contact></Contact>
     </main>
   );
 }
